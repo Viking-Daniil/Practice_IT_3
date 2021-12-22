fb4add899020faa7ca2b6f18690be7a884d8c099
Viking-Daniil <55127392+Viking-Daniil@users.noreply.github.com>
2021-12-22 20:42:57 +0300

    Add files via upload

---
+++ 4/script.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+    data() {
+        return {
+            currentTeg:'Измени моё содержимое',
+            currentTegFirst:'Измени моё содержимое',
+            isHidden: true
+        };
+    }
+}
+).mount('#app');
